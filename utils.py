afc6122aeaa3a596baca21b9d061d8f2884c9203
Irintsoanom <22336@ecam.be>
2024-02-20 11:52:41 +0100

    Functions done

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import math
+import scipy.integrate as integ
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -5,7 +7,16 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	# a = 1
+	# while (a < n):
+	# 	b = a * (a + 1)
+	# 	a += 1
+	# return b
+	res = 1
+	while (n >= 1):
+		res *= n
+		n -= 1
+	return res
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -14,7 +25,13 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = b * b - 4 * a * c
+	if delta < 0:
+		return None
+	elif delta == 0:
+		return -b / 2 * a
+	elif delta > 0:
+		return ((-b - math.sqrt(delta))/2*a, (-b + math.sqrt(delta))/2*a)
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -29,7 +46,9 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	
+	result = integ.quad(lambda x: eval(function), lower, upper)
+	return result
 
 if __name__ == '__main__':
 	print(fact(5))
